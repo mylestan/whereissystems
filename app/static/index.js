f6b0654ba0f30ff16fa3ced75f2ab2771a748b7f
Myles Tan <mylestan7@gmail.com>
2013-06-05 02:08:24 -0400

    Re-organized files, UI has map and toolbar, added bootstrap and angular.js

--- app/static/index.js
+++ app/static/index.js
@@ -1,1 +1,16 @@
 // On load, have the welcome text appear.
+$(document).ready(function (){
+	//For Google Maps
+	var mapOptions = {
+          center: new google.maps.LatLng(43.4722,-80.5472),
+          zoom: 14,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map_canvas"),
+            mapOptions);
+
+        var marker = new google.maps.Marker({
+          position: google.maps.LatLng(43.472858,-80.53993),
+          map: map
+        });
+});
