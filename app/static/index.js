fa53fd3ba76716dd3cdd2cbe4b241e1f9dac0c0d
Myles Tan <mylestan7@gmail.com>
2013-06-10 01:24:50 -0400

    custom html and css, basic framework for sidebar and popup now working.

--- app/static/index.js
+++ app/static/index.js
@@ -2,15 +2,27 @@
 $(document).ready(function (){
 	//For Google Maps
 	var mapOptions = {
-          center: new google.maps.LatLng(43.4722,-80.5472),
-          zoom: 14,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var map = new google.maps.Map(document.getElementById("map_canvas"),
-            mapOptions);
+    center: new google.maps.LatLng(43.4722,-80.5472),
+    zoom: 14,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("map-canvas"),
+      mapOptions);
 
-        var marker = new google.maps.Marker({
-          position: google.maps.LatLng(43.472858,-80.53993),
-          map: map
-        });
+  var marker = new google.maps.Marker({
+    position: google.maps.LatLng(43.472858,-80.53993),
+    map: map
+  });
 });
+
+// Show and hide the pop-op test
+function hidePopup(){
+  var popup = document.getElementById("popup");
+  popup.id = "popup-hidden";
+}
+
+
+function showPopup(){
+  var popup = document.getElementById("popup-hidden");
+  popup.id = "popup";
+}
